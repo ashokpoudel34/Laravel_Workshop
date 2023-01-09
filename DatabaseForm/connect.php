de7cd72cda40abcd942de550d385221e776ff53c
Ashok Poudel <ashokpoudel34@gmail.com>
2023-01-09 17:22:37 +0530

    new connect file

--- DatabaseForm/connect.php
+++ DatabaseForm/connect.php
@@ -1,11 +1,23 @@
 <?php
-    $host='localhost';
-    $username='ashok';
-    $password='8848';
-    $dbname = "RegForm";
-    $conn=mysqli_connect($host,$username,$password,"$dbname");
-    if(!$conn)
-        {
-          die('Could not Connect MySql Server:' .mysql_error());
-        }
+    $conn = new mysqli("localhost","ashok","8848","RegForm");
+	if ($conn -> connect_errno) 
+	{
+  	echo "Failed to connect to MySQL: " . $conn -> connect_error;
+	exit();
+	}
+	//$conn = mysqli_connect("localhost", "ashok", "8848", "RegForm");
+		//$conn = new mysqli("localhost","ashok","8848","RegForm");
+		
+		// Check connection
+		/*if (mysqli_connect_errno()) {
+  			echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ 			 exit();
+		}*/
+		
+		//if ($conn -> connect_errno) {
+  			//echo "Failed to connect to MySQL: " . $conn -> connect_error;
+			//  exit();
+		//}
 ?>
+
+
