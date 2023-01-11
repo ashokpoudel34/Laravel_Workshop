1066f4fb1c1af2d598bebe55494bc0190569e202
Ashok Poudel <ashokpoudel34@gmail.com>
2023-01-11 12:18:55 +0530

    Wesite index file added

--- VIDEO_SITE/routes/web.php
+++ VIDEO_SITE/routes/web.php
@@ -5,11 +5,14 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\VideoController;
+use App\Http\Controllers\MainController;
 
 
 
 
-Route::get('/', [HomeController::class, "index"]);
+Route::get('/', [MainController::class, "index"]);
+
+Route::get('/home', [HomeController::class, "index"]);
  
 Route::get('/contact-us', [ContactController::class, "index"]);
  
